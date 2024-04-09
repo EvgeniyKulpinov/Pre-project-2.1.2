4a604e0225dc306b337718712fa63c71f648551c
Evgeny Kulpinov <evgen2309@yandex.ru>
2024-04-09 04:09:49 +0300

    1

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld beanHW1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanHW2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanHW1 == beanHW2);
+        System.out.println(cat1 == cat2);
+    }
+}
